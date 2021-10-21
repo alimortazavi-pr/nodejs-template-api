97702ebbb499dda762ff9a101c5da9721b3e64be
alimortazavi-pr <mortazavimaneshali@gmail.com>
2021-10-22 01:01:53 +0330

    Add ImageController

---
+++ src/controllers/v1/home/imageController.js
@@ -0,0 +1,33 @@
+const controller = require("src/controllers");
+const Image = require("src/models/Image");
+const fs = require("fs");
+
+class imageController extends controller {
+  async destroy(req, res) {
+    try {
+      if (!this.isValidMongoId(res, req.params.image)) return;
+      let image = await Image.findOne({
+        _id: req.params.image,
+        user: req.user._id,
+      });
+      if (!image) {
+        return this.failed(res, "image not found", 404);
+      }
+
+      if (fs.existsSync(`public/${image.path}`)) {
+        fs.unlinkSync(`public/${image.path}`);
+        await Image.deleteOne({ _id: req.params.image });
+      } else {
+        await Image.deleteOne({ _id: req.params.image });
+      }
+
+      return res.json({
+        message: "Deleted",
+      });
+    } catch (err) {
+      this.failed(res, err.message);
+    }
+  }
+}
+
+module.exports = new imageController();
--- src/routers/v1/home.js
+++ src/routers/v1/home.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 //Controllers
 const homeController = require("src/controllers/v1/home/homeController");
+const imageController = require("src/controllers/v1/home/imageController");
 
 //middlewares
 const authenticateApiToken = require("src/middlewares/authenticateApiToken");
@@ -11,6 +12,13 @@ router.get(
   "/check-login",
   authenticateApiToken.Authenticated,
   homeController.checkLogin
+);
+
+//Images
+router.delete(
+  "/images/:image",
+  authenticateApiToken.Authenticated,
+  imageController.destroy
 );
 
 module.exports = router;
