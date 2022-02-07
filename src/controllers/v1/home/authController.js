222582e734f23c4df3acde44ffb654c46efe9f5c
alimortazavi-pr <mortazavimaneshali@gmail.com>
2022-02-07 12:33:55 +0330

    FB

--- src/controllers/v1/home/authController.js
+++ src/controllers/v1/home/authController.js
@@ -55,69 +55,82 @@ class authController extends controller {
   }
 
   async sendResetLink(req, res, next) {
-    if (!(await this.validationData(req, res))) return;
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return this.failed(res, req, "the user is not exist", 404);
-    }
+    try {
+      if (!(await this.validationData(req, res))) return;
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        return this.failed(res, req, "the user is not exist", 404);
+      }
 
-    const newPasswordReset = new PasswordReset({
-      email: req.body.email,
-      token: uniqid(),
-    });
-
-    await newPasswordReset.save();
-
-    let mailOptions = {
-      from: `"nodejs-api" <${config.url_frontend}>`, // sender address
-      to: `${newPasswordReset.email}`, // list of receivers
-      subject: "Reset Password", // Subject line
-      html: `
-                <h2>Reset Password</h2>
-                <p>Please Click on link for reset password :</p>
-                <a href="${config.url_frontend}/password-reset/${newPasswordReset.token}">Reset</a>
-            `, // html body
-    };
-
-    mail.sendMail(mailOptions, (err, info) => {
-      if (err) return this.failed(res, req, err.message);
-
-      return res.json({
-        message: "sent",
+      const newPasswordReset = new PasswordReset({
+        email: req.body.email,
+        token: uniqid(),
       });
-    });
-    return;
+
+      await newPasswordReset.save();
+
+      let mailOptions = {
+        from: `"nodejs-api" <${config.url_frontend}>`, // sender address
+        to: `${newPasswordReset.email}`, // list of receivers
+        subject: "Reset Password", // Subject line
+        html: `
+                  <h2>Reset Password</h2>
+                  <p>Please Click on link for reset password :</p>
+                  <a href="${config.url_frontend}/password-reset/${newPasswordReset.token}">Reset</a>
+              `, // html body
+      };
+
+      mail.sendMail(mailOptions, (err, info) => {
+        if (err) return this.failed(res, req, err.message);
+
+        return res.json({
+          message: "sent",
+        });
+      });
+      return;
+    } catch (err) {
+      this.failed(res, req, err.messages);
+    }
   }
 
   async resetPassword(req, res) {
-    if (!(await this.validationData(req, res))) return;
-    let field = await PasswordReset.findOne({
-      $and: [{ email: req.body.email }, { token: req.body.token }],
-    });
-    if (!field) {
-      return this.failed(res, req, "The information entered is incorrect", 403);
-    }
+    try {
+      if (!(await this.validationData(req, res))) return;
+      let field = await PasswordReset.findOne({
+        $and: [{ email: req.body.email }, { token: req.body.token }],
+      });
+      if (!field) {
+        return this.failed(
+          res,
+          req,
+          "The information entered is incorrect",
+          403
+        );
+      }
 
-    if (field.use) {
-      return this.failed(
-        res,
-        "This link has already been used to recover the password",
-        403
+      if (field.use) {
+        return this.failed(
+          res,
+          "This link has already been used to recover the password",
+          403
+        );
+      }
+
+      let user = await User.findOneAndUpdate(
+        { email: field.email },
+        { $set: { password: req.body.password } }
       );
-    }
+      if (!user) {
+        return this.failed(res, req, "Update failed", 403);
+      }
 
-    let user = await User.findOneAndUpdate(
-      { email: field.email },
-      { $set: { password: req.body.password } }
-    );
-    if (!user) {
-      return this.failed(res, req, "Update failed", 403);
+      await field.update({ use: true });
+      return res.json({
+        message: "updated",
+      });
+    } catch (err) {
+      this.failed(res, req, err.messages);
     }
-
-    await field.update({ use: true });
-    return res.json({
-      message: "updated",
-    });
   }
 }
 
