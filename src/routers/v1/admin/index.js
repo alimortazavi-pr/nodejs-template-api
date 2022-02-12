18972f58e12ff912a0b40d379742ae5a92c12a91
alimortazavi-pr <mortazavimaneshali@gmail.com>
2022-02-13 01:11:48 +0330

    FB

--- src/routers/v1/admin/index.js
+++ src/routers/v1/admin/index.js
@@ -1,8 +1,6 @@
 const router = require("express").Router();
 
 //middlewares
-const authenticateApiToken = require("src/middlewares/authenticateApiToken");
-const redirectIfNotAdmin = require("src/middlewares/redirectIfNotAdmin");
 
 const adminRouter = require("src/routers/v1/admin/admin");
 router.use("/", adminRouter);
